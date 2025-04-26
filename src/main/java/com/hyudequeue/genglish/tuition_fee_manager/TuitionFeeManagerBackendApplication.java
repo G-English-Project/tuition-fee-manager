fab1cb13b7ecc74e26591a3936af49e20aea52fd
Hyudeque <quanghuy01062004@gmail.com>
2025-04-26 22:23:29 +0700

    Init project

---
+++ src/main/java/com/hyudequeue/genglish/tuition_fee_manager/TuitionFeeManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hyudequeue.genglish.tuition_fee_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuitionFeeManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuitionFeeManagerBackendApplication.class, args);
+	}
+
+}
