f34999545f2b270efb208e39924540bff24dc25a
Hyudeque <quanghuy01062004@gmail.com>
2025-05-27 17:22:49 +0700

    Fix service error

--- src/main/java/com/hyudequeue/genglish/tuition_fee_manager/service/services/UserService.java
+++ src/main/java/com/hyudequeue/genglish/tuition_fee_manager/service/services/UserService.java
@@ -2,13 +2,14 @@ package com.hyudequeue.genglish.tuition_fee_manager.service.services;
 
 import com.hyudequeue.genglish.tuition_fee_manager.controller.model.dtos.User.request.UserCreateRequestDto;
 import com.hyudequeue.genglish.tuition_fee_manager.controller.model.dtos.User.request.UserEditRequestDto;
+import com.hyudequeue.genglish.tuition_fee_manager.controller.model.dtos.User.response.StudentAccountResponseDto;
 import com.hyudequeue.genglish.tuition_fee_manager.controller.model.dtos.User.response.UserResponseDto;
 import com.hyudequeue.genglish.tuition_fee_manager.entities.User;
 import org.springframework.data.domain.Page;
 
 public interface UserService {
     Page<UserResponseDto> GetAllStudent(int page, int size);
-    UserResponseDto CreateStudent(UserCreateRequestDto user);
+    StudentAccountResponseDto CreateStudent(UserCreateRequestDto user);
     UserResponseDto EditProfile(UserEditRequestDto user);
     void DeleteStudent(Long userId);
 }
