a42a044596558e7b5299c0231a2e245705219a75
Hyudeque <quanghuy01062004@gmail.com>
2025-08-13 17:15:47 +0700

    Update UserRepository.java

--- src/main/java/com/hyudequeue/genglish/tuition_fee_manager/repository/UserRepository.java
+++ src/main/java/com/hyudequeue/genglish/tuition_fee_manager/repository/UserRepository.java
@@ -17,6 +17,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByRole(RoleEnum roleEnum);
     Optional<User> findByEmail(String email);
     Page<User> findByRole(RoleEnum role, Pageable pageable);
 
