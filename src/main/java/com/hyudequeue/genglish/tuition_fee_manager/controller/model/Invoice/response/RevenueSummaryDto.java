91a3b72d7666cb2ca57daccd404ce10c61d38b01
Hyudeque <quanghuy01062004@gmail.com>
2025-08-24 01:10:31 +0700

    final fix

--- src/main/java/com/hyudequeue/genglish/tuition_fee_manager/controller/model/Invoice/response/RevenueSummaryDto.java
+++ src/main/java/com/hyudequeue/genglish/tuition_fee_manager/controller/model/Invoice/response/RevenueSummaryDto.java
@@ -1,12 +1,27 @@
 package com.hyudequeue.genglish.tuition_fee_manager.controller.model.Invoice.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.math.BigDecimal;
 
 @Data
-@AllArgsConstructor
 public class RevenueSummaryDto {
     private String group;
-    private BigDecimal total; 
+    private BigDecimal total;
+
+    public RevenueSummaryDto(String group, BigDecimal total) {
+        this.group = group;
+        this.total = total;
+    }
+    public RevenueSummaryDto(String group, Long total) {
+        this.group = group;
+        this.total = (total == null) ? null : BigDecimal.valueOf(total);
+    }
+    public RevenueSummaryDto(String group, Integer total) {
+        this.group = group;
+        this.total = (total == null) ? null : BigDecimal.valueOf(total.longValue());
+    }
+    public RevenueSummaryDto(String group, Double total) {
+        this.group = group;
+        this.total = (total == null) ? null : BigDecimal.valueOf(total);
+    }
 }
