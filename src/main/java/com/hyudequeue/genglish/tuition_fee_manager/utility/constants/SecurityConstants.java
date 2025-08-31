238a21f7452753e7a08ff167a33ec8e6ec7ccdc7
Hyudeque <quanghuy01062004@gmail.com>
2025-08-31 23:48:18 +0700

    Update SecurityConstants.java

--- src/main/java/com/hyudequeue/genglish/tuition_fee_manager/utility/constants/SecurityConstants.java
+++ src/main/java/com/hyudequeue/genglish/tuition_fee_manager/utility/constants/SecurityConstants.java
@@ -8,6 +8,7 @@ public class SecurityConstants {
             "http://localhost:1600",
             "https://portal-internal.gsenglish.org",
             "https://portal.gsenglish.org",
+            "http://localhost:3000"
     };
     public static final long CORS_MAX_AGE = 3600;
 
