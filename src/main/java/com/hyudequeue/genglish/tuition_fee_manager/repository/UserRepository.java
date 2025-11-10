71e7ad9e89184d8fa45da908cb2c857f55758e6d
DiamondSssssss <trongtri140604@gmail.com>
2025-11-10 19:50:16 +0700

    fixed

--- src/main/java/com/hyudequeue/genglish/tuition_fee_manager/repository/UserRepository.java
+++ src/main/java/com/hyudequeue/genglish/tuition_fee_manager/repository/UserRepository.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByRole(RoleEnum roleEnum);
-    int countByRoleAndStatus(RoleEnum role, UserStatusEnum status);
+    int countByRoleAndStudentStatus(RoleEnum role, StudentStatusEnum studentStatus);
     Optional<User> findByEmail(String email);
     Optional<User> findFirstByRole(RoleEnum role);
     Page<User> findAllByRoleAndStatus(RoleEnum role, UserStatusEnum status, Pageable pageable);
