c265741cafb6a7f124c6d9266125468a0537d865
VuQuangMinh <vuquangminh12122004@gmail.com>
2026-01-01 14:21:07 +0700

    Update FeedbackRepository.java

--- src/main/java/com/hyudequeue/genglish/tuition_fee_manager/repository/FeedbackRepository.java
+++ src/main/java/com/hyudequeue/genglish/tuition_fee_manager/repository/FeedbackRepository.java
@@ -11,8 +11,12 @@ import java.util.List;
 
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    @Query("SELECT f FROM Feedback f LEFT JOIN FETCH f.classes WHERE f.student.userId = :studentId")
     List<Feedback> findByStudentUserId(Long studentId);
+    
+    @Query("SELECT f FROM Feedback f LEFT JOIN FETCH f.classes WHERE f.teacher.userId = :teacherId")
     List<Feedback> findByTeacherUserId(Long teacherId);
-    @Query("SELECT f FROM Feedback f WHERE f.overallExperience >= 4 AND f.overallExperience <= 5 AND f.supportLevel = 'ALWAYS' ORDER BY f.createdAt DESC")
+    
+    @Query("SELECT f FROM Feedback f LEFT JOIN FETCH f.classes WHERE f.overallExperience >= 4 AND f.overallExperience <= 5 AND f.supportLevel = 'ALWAYS' ORDER BY f.createdAt DESC")
     Page<Feedback> findGoodFeedbacks(Pageable pageable);
 }
