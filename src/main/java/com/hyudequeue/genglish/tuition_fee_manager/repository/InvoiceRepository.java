75949de18bf1a53831d315121ddb4ace10828667
DiamondSssssss <trongtri140604@gmail.com>
2026-01-17 17:25:27 +0700

    hot fix dashaboard

--- src/main/java/com/hyudequeue/genglish/tuition_fee_manager/repository/InvoiceRepository.java
+++ src/main/java/com/hyudequeue/genglish/tuition_fee_manager/repository/InvoiceRepository.java
@@ -99,40 +99,33 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long>, JpaSpec
     }
     @Query("""
 select new com.hyudequeue.genglish.tuition_fee_manager.controller.model.Invoice.response.RevenueSummaryDto(
-  concat(
-    cast(function('year', coalesce(i.paidAt, i.dueDate)) as string),
-    '-',
-    lpad(cast(i.month as string), 2, '0')
-  ),
-  sum(cast(i.totalAmount as big_decimal))
+  cast(function('date_format', coalesce(i.paidAt, i.dueDate), '%Y-%m') as string),
+  sum(i.totalAmount)
 )
 from Invoice i
 where i.status = com.hyudequeue.genglish.tuition_fee_manager.entities.Enums.InvoiceStatusEnum.PAID
-group by function('year', coalesce(i.paidAt, i.dueDate)), i.month
-order by function('year', coalesce(i.paidAt, i.dueDate)) desc, i.month desc
+group by cast(function('date_format', coalesce(i.paidAt, i.dueDate), '%Y-%m') as string)
+order by cast(function('date_format', coalesce(i.paidAt, i.dueDate), '%Y-%m') as string) desc
 """)
     Page<RevenueSummaryDto> sumRevenueGroupByMonth(Pageable pageable);
 
 
+
+
     @Query("""
 select new com.hyudequeue.genglish.tuition_fee_manager.controller.model.Invoice.response.RevenueSummaryDto(
-  concat(
-    cast(function('year', coalesce(i.paidAt, i.dueDate)) as string),
-    '-',
-    lpad(cast(i.month as string), 2, '0')
-  ),
-  sum(cast(i.totalAmount as big_decimal))
+  cast(function('date_format', coalesce(i.paidAt, i.dueDate), '%Y-%m') as string),
+  sum(i.totalAmount)
 )
 from Invoice i
 where i.status = com.hyudequeue.genglish.tuition_fee_manager.entities.Enums.InvoiceStatusEnum.PAID
   and (:categoryId is null or exists (select 1 from i.categories c where c.categoryId = :categoryId))
-group by function('year', coalesce(i.paidAt, i.dueDate)), i.month
-order by function('year', coalesce(i.paidAt, i.dueDate)) desc, i.month desc
+group by cast(function('date_format', coalesce(i.paidAt, i.dueDate), '%Y-%m') as string)
+order by cast(function('date_format', coalesce(i.paidAt, i.dueDate), '%Y-%m') as string) desc
 """)
-    Page<RevenueSummaryDto> sumRevenueGroupByMonthWithCategory(
-            Pageable pageable,
-            @Param("categoryId") Long categoryId
-    );
+    Page<RevenueSummaryDto> sumRevenueGroupByMonthWithCategory(Pageable pageable, Long categoryId);
+
+
 
 
 
